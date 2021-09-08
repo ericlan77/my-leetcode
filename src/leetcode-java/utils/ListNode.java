b95ce86835b55c61adb2e1f7bc3ddc3006752fb8
ericlan77 <lan_bingliang@163.com>
2021-09-08 22:33:12 +0800

    add new solutions

---
+++ src/leetcode-java/utils/ListNode.java
@@ -0,0 +1,14 @@
+package utils;
+
+/**
+ * @ClassName: ListNode
+ * @Description: TODO
+ * @Author: Eric Lan
+ * @Date: 2020/5/4 15:51
+ * @Version: TODO
+ **/
+public class ListNode {
+    public int val;
+    public ListNode next;
+    public ListNode(int val){ this.val = val;}
+}
---
+++ src/leetcode-java/utils/TreeNode.java
@@ -0,0 +1,17 @@
+package utils;
+
+/**
+ * @ClassName: TreeNode
+ * @Description: TODO
+ * @Author: Eric Lan
+ * @Date: 2020/8/17 22:32
+ * @Version: TODO
+ **/
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int x) {
+        val = x;
+    }
+}
